7cc61c82f1caa1733c171929b65d409ed8597feb
Lucas Gimenes <lucas.gimenesbittenbinder@gmail.com>
2020-07-16 22:49:32 -0300

    adicionando desafio

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import { uuid } from "uuidv4";
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post("repositories", {
+      id: uuid(),
+      title: `Repositorio ${Date.now()}`,
+      url: "teste.url",
+      techs: ["NodeJS", "ReactJS"],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    const filteredRepositories = repositories.filter(
+      (repository) => repository.id !== id
+    );
+
+    setRepositories(filteredRepositories);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
